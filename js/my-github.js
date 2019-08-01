01ea9bf158ae35b5e2336899f959e02ea85903d6
Perasperad <you@example.com>
2019-08-01 22:27:00 +0200

    Requirement 7 - DOM & Ajax WS

--- js/my-github.js
+++ js/my-github.js
@@ -1,21 +1,37 @@
 // Write code here to communicate with Github
+var reposList = document.querySelector("#repos-list");
+var reposCount = document.querySelector("#repos-count");
+function createList(repo) {
+  var listOfRepo = document.createElement("li");
+  var linksOfRepo = document.createElement("a");
+  linksOfRepo.setAttribute("href", repo.html_url);
+  linksOfRepo.innerHTML = repo.name;
+
+  listOfRepo.appendChild(linksOfRepo);
+  reposList.appendChild(listOfRepo);
+}
 
 fetch("https://api.github.com/users/Perasperad/repos")
   .then(function(data) {
     return data.json();
   })
-  .then(function(repos) {
-    var reposList = document.querySelector("#repos-list");
-    var reposCount = document.querySelector("#repos-count");
-    reposCount.innerHTML = repos.length;
+  .then(function(myRepos) {
+    reposCount.innerHTML = myRepos.length;
 
-    repos.forEach(function(repo) {
-      var listOfRepo = document.createElement("li");
-      var linksOfRepo = document.createElement("a");
-      linksOfRepo.setAttribute("href", repo.html_url);
-      linksOfRepo.innerHTML = repo.name;
-
-      listOfRepo.appendChild(linksOfRepo);
-      reposList.appendChild(listOfRepo);
+    var input = document.getElementById("submit");
+    input.addEventListener("click", function() {
+      var userName = document.getElementById("userSearch").value;
+      if (userName == "") {
+        myRepos.forEach(createList);
+      }
+      if (userName != "") {
+        fetch("https://api.github.com/users/" + userName + "/repos")
+          .then(function(data) {
+            return data.json();
+          })
+          .then(function(repo) {
+            repo.forEach(createList);
+          });
+      }
     });
   });
