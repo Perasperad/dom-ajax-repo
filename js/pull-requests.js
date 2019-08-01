49fa886088747453f58772a91af204dab36052ef
Perasperad <you@example.com>
2019-08-01 21:23:40 +0200

    Requirement 6 - DOM and Ajax WS

--- js/pull-requests.js
+++ js/pull-requests.js
@@ -1,21 +1,38 @@
+var reqList = document.querySelector("#pull-requests-list");
+function createList(req) {
+  var listOfReq = document.createElement("li");
+  var linksOfReq = document.createElement("a");
+  linksOfReq.setAttribute("href", req.html_url);
+  linksOfReq.innerHTML = req.title;
+
+  listOfReq.appendChild(linksOfReq);
+  reqList.appendChild(listOfReq);
+}
+
 fetch("https://api.github.com/repos/codeyourfuture/js-exercises/pulls")
   .then(function(data) {
     return data.json();
   })
   .then(function(pullReq) {
-    var reqList = document.querySelector("#pull-requests-list");
+    pullReq.forEach(createList);
 
-    var filteredItems = pullReq.filter(function(ownReq) {
-      return ownReq.user.login == "Perasperad";
-    });
-
-    filteredItems.forEach(function(req) {
-      var listOfReq = document.createElement("li");
-      var linksOfReq = document.createElement("a");
-      linksOfReq.setAttribute("href", req.html_url);
-      linksOfReq.innerHTML = req.title;
-
-      listOfReq.appendChild(linksOfReq);
-      reqList.appendChild(listOfReq);
+    var input = document.getElementById("userSearch");
+    input.addEventListener("keyup", function(event) {
+      var value = event.target.value;
+      console.log(value);
+      reqList.innerHTML = "";
+      pullReq.forEach(function(pullRequest) {
+        if (value == pullRequest.user.login) {
+          var filteredItems = pullReq.filter(function(ownReq) {
+            return ownReq.user.login == value;
+          });
+          filteredItems.forEach(createList);
+        }
+      });
     });
   });
+function timedRefresh(timeoutPeriod) {
+  setTimeout("location.reload(true);", timeoutPeriod);
+}
+
+window.onload = timedRefresh(20000);
